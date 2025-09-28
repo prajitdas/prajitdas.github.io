f215fe50e93469cbd7fa2c97ca4f55f395a852c0
Prajit Kumar Das <prajdas@cisco.com>
2025-09-28 01:57:05 -0400

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,14 +3,14 @@
 
 (function (e) {
     'use strict';
-    
+
     var siteController = {
         main_fun: function () {
             // Loader fade out
             e(window).load(function () {
                 e('.loader').fadeOut('slow');
             });
-            
+
             // Vegas slideshow initialization
             e(function () {
                 e.vegas('slideshow', {
@@ -22,13 +22,13 @@
                     ]
                 })('overlay', {src: 'assets/plugins/vegas/overlays/15.png'});
             });
-            
+
             // Headline animation
             e(function () {
                 var headlineElement = e('#headLine'),
                     messages = ['WORKING..', 'LOADING..'],
                     currentIndex = -1;
-                
+
                 function animateHeadline() {
                     currentIndex = (currentIndex + 1) % messages.length;
                     headlineElement.html(messages[currentIndex])
@@ -36,18 +36,18 @@
                         .delay(1000)
                         .fadeOut(1000, animateHeadline);
                 }
-                
+
                 animateHeadline();
             });
         },
-        
+
         initialization: function () {
             siteController.main_fun();
         }
     };
-    
+
     e(document).ready(function () {
         siteController.main_fun();
     });
-    
+
 }(jQuery));
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,11 @@
 
 (function () {
     'use strict';
-    
+
     jQuery(document).ready(function (e) {
         // Debug: Check jQuery availability
         console.log('Main.js loaded. jQuery version:', e.fn.jquery);
-        
+
         // Skill level bars animation
         e('.level-bar-inner').css('width', '0');
         e(window).on('load', function () {
@@ -16,10 +16,10 @@
                 e(this).animate({width: levelWidth}, 800);
             });
         });
-        
+
         // Tooltip for level labels
         e('.level-label').tooltip();
-        
+
         // Note: RSS and GitHub Activity plugin initialization removed 
         // since target elements (#rss-feeds, #ghfeed) don't exist on the page
     });
