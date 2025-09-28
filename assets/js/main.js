e238dbbfd3b99e22bb6acc9e1cfdfc1fcac65121
Prajit Kumar Das <prajdas@cisco.com>
2025-09-27 23:02:52 -0400

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,22 +12,30 @@ jQuery(document).ready(function(e){
 	e(".level-label").tooltip();
 	
 	// RSS feeds - only if RSS plugin is available
-	function initRSSFeeds() {
+	function initRSSFeeds(retryCount) {
+		retryCount = retryCount || 0;
+		
 		if (e.fn.rss) {
-			e("#rss-feeds").rss("http://feeds.feedburner.com/TechCrunch/startups",{
-				limit:3,
-				effect:"slideFastSynced",
-				layoutTemplate:"<div class='item'>{entries}</div>",
-				entryTemplate:'<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-			});
-		} else {
+			try {
+				e("#rss-feeds").rss("http://feeds.feedburner.com/TechCrunch/startups",{
+					limit:3,
+					effect:"slideFastSynced",
+					layoutTemplate:"<div class='item'>{entries}</div>",
+					entryTemplate:'<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
+				});
+			} catch(err) {
+				console.log('RSS plugin error:', err);
+			}
+		} else if (retryCount < 20) { // Max 20 retries (2 seconds)
 			// Retry after a short delay if RSS plugin not loaded yet
-			setTimeout(initRSSFeeds, 100);
+			setTimeout(function() { initRSSFeeds(retryCount + 1); }, 100);
+		} else {
+			console.log('RSS plugin not available after retries');
 		}
 	}
 	
 	// Initialize RSS feeds
-	setTimeout(initRSSFeeds, 100);
+	setTimeout(initRSSFeeds, 50);
 	
 	// GitHub activity feed - only if GitHubActivity is available
 	function initGitHubFeed() {
